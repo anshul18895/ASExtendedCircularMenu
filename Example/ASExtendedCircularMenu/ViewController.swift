d3e06b9171c28052d83bf34189121630f581aaeb
MAC238 <MAC238@Anshul-MAC238.local>
2017-08-08 17:22:32 +0530

    Example Added

--- Example/ASExtendedCircularMenu/ViewController.swift
+++ Example/ASExtendedCircularMenu/ViewController.swift
@@ -7,11 +7,21 @@
 //
 
 import UIKit
+import ASExtendedCircularMenu
+class ViewController: UIViewController,ASCircularButtonDelegate {
 
-class ViewController: UIViewController {
-
+    @IBOutlet var shareButton: ASCircularMenuButton!
+    @IBOutlet var colourPickerButton: ASCircularMenuButton!
+    @IBOutlet var viewLbl: UILabel!
+    let colourArray: [UIColor] = [.red , .blue , .green , .yellow , .purple , .gray ,.black , .brown]
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureDynamicCircularMenuButton(button: shareButton, numberOfMenuItems: 5)
+        shareButton.menuButtonSize = .medium
+        
+        configureDraggebleCircularMenuButton(button: colourPickerButton, numberOfMenuItems: 8, menuRedius: 70, postion: .center)
+        colourPickerButton.menuButtonSize = .medium
+        colourPickerButton.sholudMenuButtonAnimate = false
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,6 +29,23 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func buttonForIndexAt(_ menuButton: ASCircularMenuButton, indexForButton: Int) -> UIButton {
+        let button: UIButton = UIButton()
+        if menuButton == shareButton{
+            button.setBackgroundImage(UIImage.init(named: "shareicon.\(indexForButton + 1)"), for: .normal)
+        }
+        if menuButton == colourPickerButton{
+            button.backgroundColor = colourArray[indexForButton]
+        }
+        return button
+    }
+    
+    func didClickOnCircularMenuButton(_ menuButton: ASCircularMenuButton, indexForButton: Int, button: UIButton) {
+        if menuButton == colourPickerButton{
+            self.viewLbl.textColor = button.backgroundColor
+        }
+        
+    }
 }
 
